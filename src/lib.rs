180f12d986c3c7f30ab57a9e4f8b2585aa18c9db
amtoine <stevan.antoine@gmail.com>
2023-08-08 10:41:00 +0200

    move kdl.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-mod kdl;
 mod nu;
 
-pub use kdl::KDL;
+use nu_plugin::{EvaluatedCall, LabeledError};
+use nu_protocol::Value;
+
+pub struct KDL;
+
+impl KDL {
+    pub fn open(&self, call: &EvaluatedCall, _input: &Value) -> Result<Value, LabeledError> {
+        eprintln!("opening KDL document");
+
+        Ok(Value::Nothing { span: call.head })
+    }
+}
