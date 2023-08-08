1f982375818c8fc8d323506c9f8a3b1309825940
amtoine <stevan.antoine@gmail.com>
2023-08-08 11:42:15 +0200

    prepare implementation of `from kdl`
    this will be simpler as a first step

--- src/nu/mod.rs
+++ src/nu/mod.rs
@@ -1,15 +1,15 @@
 use crate::KDL;
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
-use nu_protocol::{Category, PluginExample, PluginSignature, SyntaxShape, Value};
+use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
 
 impl Plugin for KDL {
     fn signature(&self) -> Vec<PluginSignature> {
-        vec![PluginSignature::build("kdl open")
-            .usage("open a file as KDL document")
-            .required("file", SyntaxShape::String, "filename")
+        vec![PluginSignature::build("from kdl")
+            .usage("TODO")
+            .input_output_type(Type::String, Type::Any)
             .plugin_examples(vec![PluginExample {
-                example: "kdl open foo.kdl".into(),
-                description: "open a KDL file".into(),
+                example: "open foo.kdl | from kdl".into(),
+                description: "TODO".into(),
                 result: None,
             }])
             .category(Category::Experimental)]
@@ -22,7 +22,7 @@ impl Plugin for KDL {
         input: &Value,
     ) -> Result<Value, LabeledError> {
         match name {
-            "kdl open" => self.open(call, input),
+            "from kdl" => self.from(call, input),
             _ => Err(LabeledError {
                 label: "Plugin call with wrong name signature".into(),
                 msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
