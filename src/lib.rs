e499c14e98c207e365931266de1f95701e10f433
amtoine <stevan.antoine@gmail.com>
2023-08-08 11:43:25 +0200

    add a base KDL traversal skeleton to `from kdl`

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,70 @@
 mod nu;
 
 use nu_plugin::{EvaluatedCall, LabeledError};
-use nu_protocol::Value;
+use nu_protocol::{Span, Value};
+
+use kdl::{KdlDocument, KdlEntry, KdlNode};
 
 pub struct KDL;
 
-impl KDL {
-    pub fn open(&self, call: &EvaluatedCall, _input: &Value) -> Result<Value, LabeledError> {
-        eprintln!("opening KDL document");
+fn build_document(document: &KdlDocument) -> Result<Value, LabeledError> {
+    eprintln!(
+        "{} -> {}",
+        document.span().offset(),
+        document.span().offset() + document.len()
+    );
 
-        Ok(Value::Nothing { span: call.head })
+    for node in document.nodes() {
+        print_node(node);
+    }
+
+    Ok(Value::Nothing {
+        span: Span::new(
+            document.span().offset(),
+            document.span().offset() + document.len(),
+        ),
+    })
+}
+
+fn print_node(node: &KdlNode) {
+    eprintln!(
+        "{}: {} -> {} ({}, {})",
+        node.name(),
+        node.span().offset(),
+        node.span().offset() + node.len(),
+        node.entries().len(),
+        match node.children() {
+            Some(_) => true,
+            None => false,
+        },
+    );
+
+    for entry in node.entries() {
+        print_entry(entry);
+    }
+
+    if let Some(children) = node.children() {
+        let _ = build_document(children);
+    }
+}
+
+fn print_entry(entry: &KdlEntry) {
+    eprintln!(
+        "{:?}: {} ({} -> {})",
+        entry.name(),
+        entry.value(),
+        entry.span().offset(),
+        entry.span().offset() + entry.len(),
+    );
+}
+
+impl KDL {
+    pub fn from(&self, _call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
+        let doc: KdlDocument = input
+            .as_string()
+            .expect("input is not a string")
+            .parse()
+            .expect("failed to parse KDL");
+        build_document(&doc)
     }
 }
